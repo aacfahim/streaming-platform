b4a9dbf97ab4cf9db05c3240d31eb44e5a1aeeab
aacfahim <aacfahim@gmail.com>
2025-05-15 19:37:45 +0600

    mail address constant

--- backend/streaming-platform /src/main/java/com/aacfahim/streaming_platform/config/Constant.java
+++ backend/streaming-platform /src/main/java/com/aacfahim/streaming_platform/config/Constant.java
@@ -3,4 +3,6 @@ package com.aacfahim.streaming_platform.config;
 public class Constant {
     public static final String SUCCESS_STATUS = "success";
     public static final String FAILED_STATUS = "failed";
+
+    public static final String MAIL_FROM = "alerts.noreply200@gmail.com";
 }
--- backend/streaming-platform /src/main/java/com/aacfahim/streaming_platform/service/impl/EmailService.java
+++ backend/streaming-platform /src/main/java/com/aacfahim/streaming_platform/service/impl/EmailService.java
@@ -3,10 +3,11 @@ package com.aacfahim.streaming_platform.service.impl;
 import com.aacfahim.streaming_platform.service.IEmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import static com.aacfahim.streaming_platform.config.Constant.MAIL_FROM;
 
 @Service
 public class EmailService implements IEmailService {
@@ -22,7 +23,7 @@ public class EmailService implements IEmailService {
         try{
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message);
-            helper.setFrom("alerts.noreply200@gmail.com");
+            helper.setFrom(MAIL_FROM);
             helper.setTo(to);
             helper.setSubject("OTP Verification Code");
             helper.setText("Your OTP code for registration is: " + otp);
